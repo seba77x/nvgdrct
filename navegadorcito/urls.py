14d5c7e936c3262aa5c6da49eb8f557d45cfe92f
seba <sebastian.salinas@ameisit.com>
2017-09-29 23:47:35 -0300

    wena pato ctm

--- navegadorcito/urls.py
+++ navegadorcito/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'profesor/(?P<idProfesor>[0-9]+)$', views.profesor,name='profesor'),
+    url(r'^login', views.login, name='login'),
+    url(r'^user', views.home, name='home'),
+
+
+    #url(r'^$', views.index, name='index'),
+    #url(r'profesor/(?P<idProfesor>[0-9]+)$', views.profesor,name='profesor'),
 ]
