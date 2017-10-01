2bad67c8eac0b7b1fd93cafe6d2a273d5effe51d
Rentaoh <falcon_p.a@hotmail.com>
2017-09-30 22:49:43 -0300

    ñee

--- navegadorcito/views.py
+++ navegadorcito/views.py
@@ -14,8 +14,8 @@ def login_acceder(request):
 		print ('*'*50)
 		print(request.POST['user'])
 		print ('*'*50)
-		if(profesor=Profesor.object.get(rut=request.POST['user']) && )
-			render 
+		if(profesor=Profesor.object.get(rut=request.POST['user']))
+			
 		if(Estudiante.objects.get(rut))
 	else:
 		return redirect ('/')
